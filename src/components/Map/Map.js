06124411b205b0a768a8e033162b3382e137149a
sacreedeception <contact@yanis-ouali.fr>
2022-01-21 22:12:37 +0100

    fait chier

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,15 +1,110 @@
 import Pion from '../Pion/Pion';
 import './map.css';
+import { Component } from 'react';
 
 
-function Map() {
-    return (
-        <div className="Map">
-            <div>
 
-            </div>
-        </div>
-    );
+
+
+class Map extends Component {
+
+
+
+    render() {
+
+        // on attends le call to action (onclick)
+        //dans le return on met l'état initial
+        return (
+            // c'est le pion qui devra être draggable
+            // disabled: boolean, if true si player 2 ?
+            <>
+
+
+
+                <table className='Map'>
+
+                    <tr>
+
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                    </tr>
+                    <tr>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                    </tr>
+                    <tr>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                    </tr>
+                    <tr>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                    </tr>    <tr>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                    </tr>
+                    <tr>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                        <td class="white"></td>
+                        <td class="black"></td>
+                    </tr>    <tr>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                    </tr>
+                    <tr>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                        <td class="white"><Pion /></td>
+                        <td class="black"><Pion /></td>
+                    </tr>
+                </table>
+            </>);
+    }
 }
 
 export default Map;
--- src/components/Pion/Pion.js
+++ src/components/Pion/Pion.js
@@ -1,10 +1,31 @@
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
+import { Component } from 'react';
+import './pion.css'
 
-function Pion() {
-    return (<>
-        <div className="Pion">
 
-        </div>
-    </>);
-}
 
+class Pion extends Component {
+
+    render() {
+
+
+        return (<>
+            <Draggable
+                handle=".handle"
+                defaultPosition={{ x: 0, y: 0 }}
+                position={null}
+                grid={[25, 25]}
+                scale={1}
+                onStart={this.handleStart}
+                onDrag={this.handleDrag}
+                onStop={this.handleStop}>
+                <span className="Pion handle">
+
+                </span>
+            </Draggable>
+        </>);
+
+    }
+};
 export default Pion;
